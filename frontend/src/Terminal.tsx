8d3bf26268c09742105a572b731b72b49b8711a8
pociej <grzegorz.pociejewski@gmail.com>
2023-11-29 22:48:31 +0100

    fix: move .env files to prop location

--- frontend/src/Terminal.tsx
+++ frontend/src/Terminal.tsx
@@ -24,7 +24,6 @@ export const TerminalComponent = () => {
   });
 
   const index = useRef(0);
-
   const socket = useRef(new WebSocket(import.meta.env.VITE_WS_URL as string));
   console.log("Launching to socket at", import.meta.env.VITE_WS_URL);
   const [command, setCommand] = useState("");
